579b82b58e8a32d3a89db8ea9d80e9d697dc506a
Linus Zetterlund <linus.zetterlund@nackademin.se>
2022-05-01 16:32:33 +0200

    Add spring initializr template with a test endpoint

---
+++ api/src/main/java/se/mbi/be2/trava/api/TravaApiApplication.java
@@ -0,0 +1,32 @@
+package se.mbi.be2.trava.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TravaApiApplication {
+
+	@Autowired
+	BuildProperties buildProperties;
+
+	@GetMapping("/")
+	public String root() {
+		return String.format("This is Trava, version %s", buildProperties.getVersion());
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s! This is the first version", name, buildProperties.getVersion());
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravaApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/se/mbi/be2/trava/api/TravaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.mbi.be2.trava.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
