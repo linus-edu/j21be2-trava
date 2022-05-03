88aab4a0d3a1b25ccdaaa6d0a073dcfec1c981cb
Linus Zetterlund <linus.zetterlund@nackademin.se>
2022-05-04 00:14:33 +0200

    Switch to gradle (keeping maven as well), improve Dockerfile

--- api/src/main/java/se/mbi/be2/trava/api/TravaApiApplication.java
+++ api/src/main/java/se/mbi/be2/trava/api/TravaApiApplication.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TravaApiApplication {
 
+	//	TODO - This causes build problems sometimes
 	@Autowired
 	BuildProperties buildProperties;
 
@@ -22,7 +23,7 @@ public class TravaApiApplication {
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s! This is the first version", name, buildProperties.getVersion());
+		return String.format("Hello %s! This is the first version", buildProperties.getVersion());
 	}
 
 	public static void main(String[] args) {
