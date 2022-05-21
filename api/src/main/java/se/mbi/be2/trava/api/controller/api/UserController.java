97e3db30bad00ba98176b7ce88ab6c7fbd4dbee2
Linus Zetterlund <linus.zetterlund@nackademin.se>
2022-05-21 22:07:32 +0200

    Add some OpenApi annotations and enable cors

--- api/src/main/java/se/mbi/be2/trava/api/controller/api/UserController.java
+++ api/src/main/java/se/mbi/be2/trava/api/controller/api/UserController.java
@@ -1,8 +1,14 @@
 package se.mbi.be2.trava.api.controller.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -20,12 +26,18 @@ public class UserController {
 
     record CreateUserDto(String username, String password) {}
 
+    @Operation(summary = "Create a new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "The user was successfully created"),
+            @ApiResponse(responseCode = "409", description = "The username already exists")}) // TODO Not actually implemented.
     @PostMapping("")
+    @ResponseStatus(code = HttpStatus.CREATED)
     public void createUser(@RequestBody CreateUserDto createUserDto) {
         LOG.info("Creating user {}", createUserDto.username);
         userService.createUser(createUserDto.username, createUserDto.password);
     }
 
+    @SecurityRequirement(name = "bearerAuth")
     @GetMapping("")
     public Iterable<UserEntity> getUsers(@AuthenticationPrincipal UserDetails user) {
         LOG.info("Sending list of all users to user='{}'", user.getUsername());
--- api/src/main/java/se/mbi/be2/trava/api/security/ApiSecurityConfiguration.java
+++ api/src/main/java/se/mbi/be2/trava/api/security/ApiSecurityConfiguration.java
@@ -1,5 +1,9 @@
 package se.mbi.be2.trava.api.security;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +13,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class ApiSecurityConfiguration {
@@ -22,7 +31,7 @@ public class ApiSecurityConfiguration {
         http
                 .antMatcher("/api/**")
                 .csrf().disable()
-                .cors().disable()
+                .cors().and()
                 .formLogin().disable()
                 .authorizeRequests(authz -> authz
                         .antMatchers(HttpMethod.POST, "/api/users").permitAll()
@@ -33,6 +42,28 @@ public class ApiSecurityConfiguration {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedOrigins(List.of("https://editor.swagger.io/"));
+        configuration.setAllowedMethods(List.of("GET", "POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Configuration
+    @OpenAPIDefinition(info = @Info(title = "Trava API", version = "v1"))
+    @SecurityScheme(
+            name = "bearerAuth",
+            type = SecuritySchemeType.HTTP,
+            bearerFormat = "JWT",
+            scheme = "bearer"
+    )
+    public class OpenApi30Config {
     }
 
 //    @Order(2)
